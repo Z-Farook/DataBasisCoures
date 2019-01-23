88101b7b46dc0fc99cc18c7669fee07fabbf225f
Z-Farook <46790696+Z-Farook@users.noreply.github.com>
2019-01-23 12:34:36 +0100

    Add files via upload

---
+++ MapReduce_code/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AirportDb;
+
+namespace ExamPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Company c = new Company("asdf", 12, 12.00);
+
+            AirportDatabase aiportDb = AirportDatabase.Test();
+            foreach (var item in aiportDb.Query1())
+            {
+                System.Console.WriteLine(item.CompanyName);
+
+            }
+
+
+
+        }
+    }
+}
